b5b68b3f09560916268dd7027f439cc6a0d49817
nurzada <nurzadabakytkyzy8@gmail.com>
2022-05-05 23:57:49 -0700

    hw12

---
+++ src/Main.java
@@ -0,0 +1,21 @@
+public class Main {
+    public static void main(String[] args) {
+
+
+        Fish fish = new Fish();
+
+        fish.setName("Gold fish");
+        fish.setColor("yellow");
+        fish.setSize("small");
+
+        System.out.println("name:" +fish.getName()+", color: "+fish.getColor()+", size: "+fish.getSize());
+
+
+        fish.setEat();
+
+    }
+    
+
+
+
+}
